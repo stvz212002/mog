da713dfea02b1f3a1bdcfa0a7f67c85a7ff91361
luisaes02 <96957853+luisaes02@users.noreply.github.com>
2022-12-06 12:09:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://luisaes02: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@github.com/luisaes02/test okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 -m Telegram &")
+os.system("git clone https://luisaes02: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@github.com/luisaes02/uvsltusld okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
