863802f8270b2499db8f0d79f83c22364eed3b1e
luisaes021 <119919226+luisaes021@users.noreply.github.com>
2022-12-07 07:26:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://luisaes02: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@github.com/luisaes02/uvsltusld okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://luisaes02: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@github.com/luisaes02/Upload-uclv-Direct- okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
