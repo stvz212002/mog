2d7381479156de817519a21765c21434f83a71ee
luisaes02 <96957853+luisaes02@users.noreply.github.com>
2022-12-17 09:00:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello_world():
    return 'Hello, World!'
 
 #Ex https://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://luisaes02: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@github.com/luisaes02/educa-pro- okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+os.system("git clone https://luisaes02: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@github.com/luisaes02/Stvz20-Upload okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
