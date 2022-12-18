f93ddcffcf564045f6c77d97f6af54e2b91aa912
luisaes02 <96957853+luisaes02@users.noreply.github.com>
2022-12-18 17:02:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return 'Gracias Por mantener el bot funcionando!'
+   return 'Gracias Por mantener el bot funcionando! Ya puede Subir|Descargar si Archivo https://t.me/Stvz_UploadPro_bot'
 
 #Exthttps://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
 os.system("git clone https://luisaes02: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@github.com/luisaes02/Stvz20-Upload okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
