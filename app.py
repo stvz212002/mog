5e42e96c7970832e3b2c2ba4e30ebe6ce94155bb
stvz212002 <133266723+stvz212002@users.noreply.github.com>
2023-10-31 08:07:53 -0700

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,5 @@ def hello_world():
    return 'Gracias Por mantener el bot funcionando! Ya puede Subir|Descargar si Archivo https://t.me/Stvz_UploadPro_bot'
 
 #Exthttps://Itz-zaid:ghp_147bkkabcdefgh@github.com/Itz-zaid/anything
-os.system("git clone https://luisaes02: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@github.com/luisaes02/Stvz20-Upload okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 main.py &")
+#os.system("git clone https://stvz212002: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@github.com/stvz212002/mejorado okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
+os.system("git clone https://github.com/stvz212002/mejorado okk && cd okk && pip3 install -U -r requirements.txt && nohup python3 bot.py &")
